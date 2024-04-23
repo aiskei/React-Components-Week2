5858e1e90f906e8c0b66e73754f59c90d1dc61d1
AishaKeita <aishacassel@gmail.com>
2024-04-23 12:58:01 +0200

    Install react Learn about JSX component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+    const name = 'John Doe'
+    return(
+        <div>
+            <h1>React component</h1>
+            <p>This is a JSX component</p>
+            {name}
+        </div>
+    )
+}
+
+export default App;

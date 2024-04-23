9e3217fd3a920f5fb48fba7a6b625e0542b64aa9
AishaKeita <aishacassel@gmail.com>
2024-04-23 13:03:59 +0200

    Fragemet or array with variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 const App = () => {
     const name = 'John Doe'
-    return(
+
+    //fragemet or array with variables
+    const heading2 = <h2>This is heading 2</h2>
+    return (
         <>
             <h1>React component</h1>
+            {heading2}
             <p>This is a JSX component</p>
             {name}
         </>
     )
 }
 
-export default App;
+export default App
